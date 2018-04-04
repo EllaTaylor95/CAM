e48660262c8e790701258727c0d07952106bce39
Ella Taylor <pmxet1@nottingham.ac.uk>
2018-04-04 16:40:05 +0100

    Still looking for bugs

--- CW_2/CW2_P3B.cpp
+++ CW_2/CW2_P3B.cpp
@@ -55,23 +55,23 @@ return K*exp(-r*t);
 }
 
 //Assign vectors according to discretised Black Scholes Eq/
-void assign_vectors(int m, int n, double K, double R, double r, double sigma_sq, double delta_t, double* x, double* f, double* U, double* diag, double* upper, double* lower, double* U_prev)
+void assign_vectors(int m, int n, double K, double R, double r, double sigma_sq, double delta_t, double* f, double* U, double* diag, double* upper, double* lower, double* U_prev)
     {
 double h = R/(double)(m);
 
-f[0] = f_0((double)(n)*delta_t,K,r)*(-delta_t*sigma_sq*pow(0,2)/(2*pow(h,2)));
-f[m-1] = u_exact(R,(double)(n)*delta_t,r,sigma_sq,K)*(-delta_t*r*R/h - delta_t*sigma_sq*pow(R,2)/(2*pow(h,2)));
+f[0] = f_0((double)(n)*delta_t,K,r)*(-delta_t*sigma_sq/2);
+f[m-1] = u_exact(R,(double)(n)*delta_t,r,sigma_sq,K)*(-delta_t*r*m - delta_t*sigma_sq*pow(m,2));
 
 for (int i=0; i<m; i++)
     {
-    diag[i] = 1 + delta_t*r + delta_t*r*x[i]/h + delta_t*sigma_sq*pow(x[i],2)/pow(h,2);
+    diag[i] = 1 + delta_t*r + delta_t*r*i + delta_t*sigma_sq*pow(i,2);
     U_prev[i] = U[i] + f[i];
     }
 
 for (int i=0; i<m-1; i++)
     {
-    lower[i] = - delta_t*sigma_sq*pow(x[i],2)/(2*pow(h,2));
-    upper[i] = - delta_t*r*x[i]/h - delta_t*sigma_sq*pow(x[i],2)/(2*pow(h,2));
+    lower[i] = - delta_t*sigma_sq*pow(i,2)/2.0;
+    upper[i] = - delta_t*r*i - delta_t*sigma_sq*pow(i,2)/2.0;
     }
 
     }
@@ -100,19 +100,15 @@ U = new double[m];
 double* f;
 f = new double[m];
 
-double* x;
-x = new double[m];
-
 
 for(int i =0; i<m; i++)
     {
     //Assigning ICs
-    x[i] = (i+1)*h; //Shifted across by one as U(0) already known
     f[i] = 0;
-    U[i] = g(x[i],K);
+    U[i] = g(h*(i+1),K);
     }
 
-assign_vectors(m,0,K,R,r,sigma_sq,delta_t,x,f,U,diag,upper,lower,U_prev);
+assign_vectors(m,0,K,R,r,sigma_sq,delta_t,f,U,diag,upper,lower,U_prev);
 
 for(int n = 0; n<num_of_time_steps+1; n++)
     {
@@ -126,7 +122,7 @@ for(int n = 0; n<num_of_time_steps+1; n++)
         }
     std::cout<<std::setw(1)<<m+1<<std::setw(15)<<u_exact(R,(double)(n)*delta_t,r,sigma_sq,K)<<std::endl;
 
-    assign_vectors(m,n,K,R,r,sigma_sq,delta_t,x,f,U,diag,upper,lower,U_prev);
+    assign_vectors(m,n,K,R,r,sigma_sq,delta_t,f,U,diag,upper,lower,U_prev);
     }
 
 //Define error as max (e_N) = max error at final time
@@ -135,7 +131,7 @@ double Ei, E_max = 0;
 
 for(int i =0; i<m; i++)
     {
-    Ei = fabs(U[i] - u_exact(x[i],T,r,sigma_sq,K));
+    Ei = fabs(U[i] - u_exact(h*(i+1),T,r,sigma_sq,K));
 
     if(E_max < Ei)
         {
