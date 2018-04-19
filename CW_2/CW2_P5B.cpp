452a34dfd1907e2297e2f80ad18e230af273c802
Ella Taylor <pmxet1@nottingham.ac.uk>
2018-04-19 13:15:05 +0100

    Working code for P5

--- CW_2/CW2_P5B.cpp
+++ CW_2/CW2_P5B.cpp
@@ -4,8 +4,8 @@
 
 double alpha(int i)
 {
-int m = 16, num_of_iterations = 10;
-double h = 1.0/(double)(m), T = 5.0, delta_t = T/num_of_iterations, omega = 1.8, r = 0.05, sigma_sq = 0.25;
+int m = 16, num_of_iterations = 20;
+double R = 300.0, h = R/(double)(m), T = 5.0, delta_t = T/(double)(num_of_iterations), omega = 1.8, r = 0.05, sigma_sq = 0.25;
 
 double alpha = 1.0 + r*delta_t*(1.0+i) + sigma_sq*pow(i,2)*delta_t;
 
@@ -14,18 +14,18 @@ return alpha;
 
 double beta(int i)
 {
-int m = 16,num_of_iterations = 10;
-double h = 1.0/(double)(m), T = 5.0, delta_t = T/num_of_iterations, omega = 1.8, r = 0.05, sigma_sq = 0.25;
+int m = 16,num_of_iterations = 20;
+double R = 300.0, h = R/(double)(m), T = 5.0, delta_t = T/(double)(num_of_iterations), omega = 1.8, r = 0.05, sigma_sq = 0.25;
 
-double beta = -0.5*delta_t*sigma_sq*pow(i,2);
+double beta = -0.5*delta_t*sigma_sq*pow(i,2) - r*(i)*delta_t;
 
 return beta;
 }
 
 double gamma(int i)
 {
-int m = 16,num_of_iterations = 10;
-double h = 1.0/(double)(m), T = 5.0, delta_t = T/num_of_iterations, omega = 1.8, r = 0.05, sigma_sq = 0.25;
+int m = 16,num_of_iterations = 20;
+double R = 300.0, h = R/(double)(m), T = 5.0, delta_t = T/(double)(num_of_iterations), omega = 1.8, r = 0.05, sigma_sq = 0.25;
 
 double gamma = -0.5*delta_t*sigma_sq*pow(i,2);
 
@@ -35,12 +35,12 @@ return gamma;
 int main()
 {
 
-int m = 16,num_of_iterations = 10;
-double h = 1.0/(double)(m), T = 5.0, delta_t = T/num_of_iterations, omega = 1.8, r = 0.05, sigma_sq = 0.25, K = 100;
+int m = 16,num_of_iterations = 20;
+double R = 300.0, h = R/(double)(m), T = 5.0, delta_t = T/(double)(num_of_iterations), omega = 1.8, r = 0.05, sigma_sq = 0.25, K = 100;
 
-double u_bar[m], u[m], g[m];
+double u_bar[m+1], u[m+1], g[m+1];
 
-for (int i = 0; i<m; i++)
+for (int i = 0; i<m+1; i++)
     {
     u[i] = 0.0;
 
@@ -50,11 +50,12 @@ for (int i = 0; i<m; i++)
         }
     else
         {
-        g[i] = 0;
+        g[i] = 0.0;
         }
     }
 
-u[0] = u[0] - K*gamma(1); //this is wrongggg
+u[0] = K;
+u[m] = 0.0;
 
 int counter =0;
 
@@ -63,17 +64,15 @@ while(counter<num_of_iterations)
 
     std::cout<<"n"<<std::setw(4)<<"u"<<std::endl;
 
-    u_bar[0] = u[1]/gamma(1);
+    for(int i = 1; i<m; i++)
+        {
+        u_bar[i] = (1.0/alpha(i))*( u[i] - gamma(i-1)*u[i-1] - beta(i+1)*u[i+1] );
+        }
 
     for(int i = 1; i<m; i++)
         {
-        u_bar[i] = (1.0/alpha(i))*( u[i] - gamma(i)*u[i-1] - beta(i)*u[i+1] );
-        }
 
-    for(int i = 0; i<m; i++)
-        {
-
-        if (g[i] >= (omega*u_bar[i]+(1-omega)*u[i]) )
+        if (g[i] >= (omega*u_bar[i]+(1.0-omega)*u[i]) )
             {
             u[i] = g[i];
             }
@@ -85,13 +84,11 @@ while(counter<num_of_iterations)
     counter ++;
 
     std::cout<<counter<<std::endl;
-    for (int i = 0; i<m; i++)
+    for (int i = 0; i<m+1; i++)
         {
         std::cout<<std::setw(5)<<u[i]<<std::endl;
         }
     }
-
-
 
 return 0;
 }
